a27e1b75191a2ad21c66ad782a296a4dc65ed7fc
= <=>
2020-04-24 18:20:51 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,50 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+
+Vue.config.productionTip = false;
+Vue.use(Vuetify);
+
+new Vue({
+  el: "#app",
+  vuetify: new Vuetify(),
+  data: () => ({
+    dialog: false,
+    drawer: null,
+    items: [
+      { icon: "contacts", text: "Contacts" },
+      { icon: "history", text: "Frequently contacted" },
+      { icon: "content_copy", text: "Duplicates" },
+      {
+        icon: "keyboard_arrow_up",
+        "icon-alt": "keyboard_arrow_down",
+        text: "Labels",
+        model: true,
+        children: [{ icon: "add", text: "Create label" }]
+      },
+      {
+        icon: "keyboard_arrow_up",
+        "icon-alt": "keyboard_arrow_down",
+        text: "More",
+        model: false,
+        children: [
+          { text: "Import" },
+          { text: "Export" },
+          { text: "Print" },
+          { text: "Undo changes" },
+          { text: "Other contacts" }
+        ]
+      },
+      { icon: "settings", text: "Settings" },
+      { icon: "chat_bubble", text: "Send feedback" },
+      { icon: "help", text: "Help" },
+      { icon: "phonelink", text: "App downloads" },
+      { icon: "keyboard", text: "Go to the old version" }
+    ]
+  }),
+  router,
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/views/Packs.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="packs">
+    <h1>Sticazzi</h1>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Packs"
+  // components: {}
+};
+</script>
