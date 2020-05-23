5c4014e4080a6d6245920ab98d8f7c21b003d5f6
= <=>
2020-05-23 18:42:23 +0200

    server heroku + package.json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/'));
 });
 var port = process.env.PORT || 5000;
 app.listen(port);
