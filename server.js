07d91d53aec5370edf8f9d8119fa3ae098b76acd
= <=>
2020-05-23 19:01:37 +0200

    server heroku + package.json

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');
-var path = require('path');
+var serveStatic = require('serve-static');
 var app = express();
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
-app.listen(port);
+app.listen(port); console.log('server started ' + port);
